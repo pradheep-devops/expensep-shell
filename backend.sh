2e4fc2faadaf0e9681a6b0c2354c5050d66f9f2a
pradeep <pradeepdevops93@gmail.com>
2024-06-12 14:09:28 -0500

    backend script and service file

--- backend.sh
+++ backend.sh
@@ -1,6 +1,7 @@
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
+cp backend.service /etc/systemd/system/backend.service
 useradd expense
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
@@ -10,7 +11,6 @@ unzip /tmp/backend.zip
 cd /app
 npm install
 
-cp backend.service /etc/systemd/system/backend.service
 systemctl daemon-reload
 systemctl enable backend
 systemctl start backend

612bcc4e9b2ec4d51fb221f23273a9a8d7832799
pradeep <pradeepdevops93@gmail.com>
2024-06-12 14:17:11 -0500

    backend script remove content in app dir

--- backend.sh
+++ backend.sh
@@ -1,8 +1,10 @@
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
-cp backend.service /etc/systemd/system/backend.service
+
 useradd expense
+cp backend.service /etc/systemd/system/backend.service
+rm -rf /app
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 cd /app
